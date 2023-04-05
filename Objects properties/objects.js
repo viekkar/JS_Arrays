5bdbdda197c708092a83b61b3e24b1607be952d6
ikkarbhagwan <ikkarbhagwan@gmail.com>
2023-04-05 23:32:39 +0530

    objects revision
    extra

---
+++ Objects properties/objects.js
@@ -0,0 +1,27 @@
+
+let person = {
+
+    FirstName :'Bhagwan',
+    LastName :'Ikkar'
+};
+
+
+document.write(person['FirstName']); //here we can declare or show thr value of object
+/* 
+let person = {
+
+    FirstName :'Bhagwan',
+    LastName :'Ikkar'
+};
+
+console.log(person.age); // when we declare new named object (age) its shows undefined 
+
+let person = {
+
+    FirstName :'Bhagwan',
+    LastName :'Ikkar'
+};
+
+
+document.write(person['FirstName']); //here we can declare or show thr value of object
+ */
