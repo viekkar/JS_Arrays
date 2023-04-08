ae4c5fd43d96e87e35c396efcf4c6722c537d5d2
ikkarbhagwan <ikkarbhagwan@gmail.com>
2023-04-08 17:41:27 +0530

    Find Even & Odd Numbers with Loops
    Find Even & Odd Numbers with Loops small practice

---
+++ Find Even & Odd Numbers with Loops/index.js
@@ -0,0 +1,13 @@
+// for (var a = 1; 1 <= 10; a++) {
+//   if (a%2 == 0) {
+//     console.log(a + "<br>");
+//   }
+// }
+
+
+for (var a = 1; a <=100; a++ ){
+  if(a %2 !=0){
+    document.write(a + "<br>");
+  }
+
+}
