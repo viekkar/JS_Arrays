b85e3a63f4a9539771ad13081f20ef6d280dd262
ikkarbhagwan <ikkarbhagwan@gmail.com>
2023-04-19 14:08:01 +0530

    method and contructor together

--- OOPs in JS/index.js
+++ OOPs in JS/index.js
@@ -1,12 +1,37 @@
-class a 
-{
-show()
-{
+// class a 
+// {
+// show()
+// {
 
-    document.write("Hello i am in a class...");
+//     document.write("Hello i am in a class...");
+// }
+
+// }
+
+// let ao = new a();
+// ao.show();
+
+// let ao2 = new A();
+
+
+class A {
+
+    hello()
+    {
+        document.write("HEllo im method");
+    }
+
+    constructor(x, y)
+
+          {
+
+             let z = x + y;
+               document.write("Hello im a contructor " + z + "<br>");
+
+          }
+
+
 }
 
-}
-
-let ao = new a();
-ao.show();
+let obj = new A(2,4);
+obj.hello();
