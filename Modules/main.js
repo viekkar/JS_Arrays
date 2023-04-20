98c455666cf68a55d7cc93fd628cb0ed8455c198
ikkarbhagwan <ikkarbhagwan@gmail.com>
2023-04-20 12:24:14 +0530

    Module and how to link multiple files process

---
+++ Modules/main.js
@@ -0,0 +1,24 @@
+export let n = "Bhagwan";
+
+export function show(name) 
+
+
+
+{
+    console.log("Hello evryone....." +name);
+}
+
+
+
+
+export class myclass
+
+{
+
+    constructor()
+    {
+        console.log("Hello Im Bhaguuuu");
+    }
+
+}
+
---
+++ Modules/main2.js
@@ -0,0 +1,11 @@
+import {n,show,myclass} from "./main.js";
+// import {show} from "./main.js";
+// import{myclass} from"./main.js";
+
+// console.log("hello ..." +n);
+
+document.body.innerHTML=("hello ..." +n);
+
+show("Ikkar");
+
+let ao=new myclass();
